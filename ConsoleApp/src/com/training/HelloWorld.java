f5a590069e74e752356627f9ca5b978995fd275c
gandharv <sirohi.gandharv@gmail.com>
2020-06-30 18:32:20 +0530

    Added a comment

--- ConsoleApp/src/com/training/HelloWorld.java
+++ ConsoleApp/src/com/training/HelloWorld.java
@@ -2,6 +2,7 @@ package com.training;
 
 public class HelloWorld {
     public static void main(String[] args){
+        //only for testing purpose
         int i = 100;
         ++i;
         System.out.println("Hello World!!");
