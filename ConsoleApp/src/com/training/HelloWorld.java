43fa7c7cfa462f6e91fdfd061d828d32fb245da7
gandharv <sirohi.gandharv@gmail.com>
2020-06-30 17:53:30 +0530

    added int i

--- ConsoleApp/src/com/training/HelloWorld.java
+++ ConsoleApp/src/com/training/HelloWorld.java
@@ -2,6 +2,8 @@ package com.training;
 
 public class HelloWorld {
     public static void main(String[] args){
+        int i = 100;
+        ++i;
         System.out.println("Hello World!!");
     }
 }
